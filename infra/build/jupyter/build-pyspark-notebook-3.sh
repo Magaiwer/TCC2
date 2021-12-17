648b7273691945fb2069d23820ed2cb7db441160
Magaiwer <magaiwer@hotmail.com.br>
2021-12-17 15:47:06 -0300

    Delete build-pyspark-notebook-3.sh

--- infra/build/jupyter/build-pyspark-notebook-3.sh
+++
@@ -1,6 +0,0 @@
-docker build . -t magaiwer/spark-py:3.0.0
-# tag image
-docker tag magaiwer/spark-py:3.0.0 magaiwer/pyspark-notebook:3.0.0
-
-# push image to registry
-docker push magaiwer/pyspark-notebook:3.0.0
